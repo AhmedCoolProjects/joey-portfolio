5714127f731230cd67419da0957aaa20149ffc8e
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-08-30 16:19:15 +0100

    adding alt to img and removing unused import

--- src/lib/components/timeline/customTimelineItem.svelte
+++ src/lib/components/timeline/customTimelineItem.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { TimelineItem } from 'flowbite-svelte';
-	import { Icon } from 'flowbite-svelte-icons';
 	export let title: string;
 	export let date: string;
 	export let description: string;
@@ -11,7 +10,7 @@
 		<span
 			class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-primary-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-primary-900"
 		>
-			<img src="/favicon.png" class="w-3 h-3 text-primary-600 dark:text-primary-400" />
+			<img alt={title} src="/favicon.png" class="w-3 h-3 text-primary-600 dark:text-primary-400" />
 		</span>
 	</svelte:fragment>
 	<p class="text-base font-normal text-gray-500 dark:text-gray-400">
